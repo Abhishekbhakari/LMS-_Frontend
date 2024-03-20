aa802a566804bce9f0d1ab1b2db257d837e17f07
AbhishekBhakari <bhakariabhi@gmail.com>
2024-03-20 21:44:37 +0530

    added course description page & Course creation by Admin

--- src/Redux/Slices/CourseSlice.js
+++ src/Redux/Slices/CourseSlice.js
@@ -1,23 +1,61 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import axiosInstance from "../../Helpers/axiosInstance"
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
+
+import axiosInstance from "../../Helpers/axiosInstance";
 
 const initialState = {
     courseData: []
 }
 
-export const getAllCourses = createAsyncThunk("/course/get" , async () => {
+export const getAllCourses = createAsyncThunk("/course/get", async () => {
     try {
         const response = axiosInstance.get("/courses");
         toast.promise(response, {
-            loading: "Loading Course Data ...",
-            success: "courses loaded Sucessfully",
-            error: "Failed to get the courses"
+            loading: "loading course data...",
+            success: "Courses loaded successfully",
+            error: "Failed to get the courses",
         });
 
         return (await response).data.courses;
+    } catch(error) {
+        toast.error(error?.response?.data?.message);
+    }
+}); 
 
-    } catch (error) {
+export const deleteCourse = createAsyncThunk("/course/delete", async (id) => {
+    try {
+        const response = axiosInstance.delete(`/courses/${id}`);
+        toast.promise(response, {
+            loading: "deleting course ...",
+            success: "Courses deleted successfully",
+            error: "Failed to delete the courses",
+        });
+
+        return (await response).data;
+    } catch(error) {
+        toast.error(error?.response?.data?.message);
+    }
+}); 
+
+export const createNewCourse = createAsyncThunk("/course/create", async (data) => {
+    try {
+        let formData = new FormData();
+        formData.append("title", data?.title);
+        formData.append("description", data?.description);
+        formData.append("category", data?.category);
+        formData.append("createdBy", data?.createdBy);
+        formData.append("thumbnail", data?.thumbnail);
+
+        const response = axiosInstance.post("/courses", formData);
+        toast.promise(response, {
+            loading: "Creating new course",
+            success: "Course created successfully",
+            error: "Failed to create course"
+        });
+
+        return (await response).data
+
+    } catch(error) {
         toast.error(error?.response?.data?.message);
     }
 });
@@ -28,7 +66,7 @@ const courseSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder.addCase(getAllCourses.fulfilled, (state, action) => {
-            if (action.payload) {
+            if(action.payload) {
                 state.courseData = [...action.payload];
             }
         })
