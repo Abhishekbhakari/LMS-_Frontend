4f2bad7610e25b0d98fb166b239ce35a9a53a09d
AbhishekBhakari <bhakariabhi@gmail.com>
2024-04-08 12:41:55 +0530

    added RozorpaySlice and payment Checkout Page

--- src/Pages/Course/CourseDescription.jsx
+++ src/Pages/Course/CourseDescription.jsx
@@ -1,33 +1,37 @@
-import { useLocation } from "react-router-dom";
-import HomeLayout from "../../Layouts/HomeLayout";
 import { useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import HomeLayout from "../../Layouts/HomeLayout";
 
 function CourseDescription() {
 
     const { state } = useLocation();
-    const { role } = useSelector((state) => state.auth);
+    const navigate = useNavigate();
 
+    const { role, data } = useSelector((state) => state.auth);
 
     return (
         <HomeLayout>
-            <div className=" min-h-[90vh] pt-12 px-20 flex flex-col items-center justify-center text-white">
-                <div className=" grid grid-cols-2 gap-10 py-10 relative">
-                    <div className=" space-y-5">
+            <div className="min-h-[90vh] pt-12 px-20 flex flex-col items-center justify-center text-white">
+                <div className="grid grid-cols-2 gap-10 py-10 relative">
+                    <div className="space-y-5">
                         <img 
-                            className=" w-full h-64"
+                            className="w-full h-64"
+                            alt="thumbnail"
                             src={state?.thumbnail?.secure_url}
-                            alt="thumbnail" 
                         />
-                        <div className=" space-y-4">
-                            <div className=" flex items-center justify-between text-xl">
-                                <p className=" font-semibold">
+
+                        <div className="space-y-4">
+                            <div className="flex flex-col items-center justify-between text-xl">
+
+                                <p className="font-semibold">
                                     <span className="text-yellow-500 font-bold">
-                                        Total Lectures : {" "}
+                                        Total lectures : {" "}
                                     </span>
                                     {state?.numberOfLectures}
                                 </p>
 
-                                <p className=" font-semibold">
+                                <p className="font-semibold">
                                     <span className="text-yellow-500 font-bold">
                                         Instructor : {" "}
                                     </span>
@@ -36,35 +40,34 @@ function CourseDescription() {
 
                             </div>
 
-                            { role === "ADMIN" || data?.subscription?.status === "ACTIVE" ? (
-                                 <button className=" bg-yellow-600 text-xl rounded-md font-bold px-5 py-3 w-full hover:bg-yellow-500 transition-all ease-in-out duration-300">
-                                        Watch Lectures
-                                    </button>
-                                ):(
-                                    <button className=" bg-yellow-600 text-xl rounded-md font-bold px-5 py-3 w-full hover:bg-yellow-500 transition-all ease-in-out duration-300">
+                            { role === "ADMIN" || data?.subscription?.status === "active" ? (
+                                <button onClick={() => navigate("/course/displaylectures", {state: {...state}})} className="bg-yellow-600 text-xl rounded-md font-bold px-5 py-3 w-full hover:bg-yellow-500 transition-all ease-in-out duration-300">
+                                    Watch lectures
+                                </button>
+                                ) : (
+                                    <button onClick={() => navigate("/checkout")} className="bg-yellow-600 text-xl rounded-md font-bold px-5 py-3 w-full hover:bg-yellow-500 transition-all ease-in-out duration-300">
                                         Subscribe
                                     </button>
                                 )
 
                             }
-
                         </div>
-                        
+                       
+
                     </div>
 
-                    <div className=" space-y-2 text-xl">
-                        <h1 className=" text-3xl font-bold text-yellow-500 mb-5 text-center">
+                    <div className="space-y-2 text-xl">
+                        <h1 className="text-3xl font-bold text-yellow-500 mb-5 text-center">
                             {state?.title}
                         </h1>
 
-                        <p className=" text-yellow-500">Course description: </p>
-                        <p>{state?.discription}</p>
+                        <p className="text-yellow-500">Course description: </p>
+                        <p>{state?.description}</p>
                     </div>
                 </div>
-
             </div>
         </HomeLayout>
-    )
+    );
 }
 
 export default CourseDescription;
