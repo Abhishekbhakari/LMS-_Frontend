5e0762bf293da1bb016baf6b65c0849e4b5ec480
Abhishek Bhakari <130546310+Abhishekbhakari@users.noreply.github.com>
2024-04-22 16:43:23 +0530

    Update axiosInstance.js

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://lms-backend-rouge.vercel.app/api/v1";
+const BASE_URL = "https://lms-frontend-dusky-delta.vercel.app/api/v1";
 
 const axiosInstance = axios.create();
 
