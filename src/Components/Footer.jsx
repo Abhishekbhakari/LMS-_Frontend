ef03e68f6a6ec7d36651ecf7c03695219b7b8d50
AbhishekBhakari <bhakariabhi@gmail.com>
2024-04-23 11:43:02 +0530

    Finished project

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,6 +5,7 @@ function Footer() {
     const year = currentDate.getFullYear();
     return(
         <>
+        //footer
             <footer  className="relative left-0 bottom-0 h-[10vh] py-5 flex flex-col sm:flex-row items-center justify-between text-white bg-gray-800 sm:px-20 ">
                 <section className="text-lg">
                     Copyright {year} | all rights reserved
